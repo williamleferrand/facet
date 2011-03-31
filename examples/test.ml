d6f34f944399460a983fe22ba2347b906910ef28
wleferrand <william@corefarm.com>
2011-03-30 17:05:25 -0700

    writing an extension syntax ..

--- examples/test.ml
+++ examples/test.ml
@@ -1,8 +1,23 @@
 (* Just a test *)
 
-type e = 
-    {
-      compensation: bool ; 
-      keywords : string list ; 
-    } with facet
-      
+(* Best practise: write the first accessed labels first *)
+
+
+module type ELT = 
+  sig
+    type t
+    type t_structure 
+  end
+
+module E =
+  struct 
+    type t = 
+	{
+	  
+	  compensation: bool; 
+
+	  keywords : string list ; 
+	  	  
+	} with facet
+	    
+  end
--- lib/syntax/pa_facet.ml
+++ lib/syntax/pa_facet.ml
@@ -13,11 +13,34 @@ struct
 end
 
 
+
+
 module Make (Syntax : Sig.Camlp4Syntax) =
 struct
   open Sig
   include Syntax
 
+  (* Misc helpers ********************************************************************)
+
+  exception BadType
+    
+  (* Create the type that handles the weak pointers **********************************)
+
+  let rec iter_over_fields_ _loc tp = 
+    match tp with 
+      | <:ctyp< $lid:_$ : string >> | <:ctyp< $lid:_$ : list string >> -> <:ctyp< Hashtbl.t string (Weak.t t) >>  
+      | <:ctyp< $lid:_$ : bool >> -> <:ctyp< Hashtbl.t bool (Weak.t t) >>  
+      | <:ctyp< $lid:_$ : bool; $tp$ >> -> <:ctyp< Hashtbl.t bool ($iter_over_fields_ _loc tp$) >>  
+
+  let iter_over_fields _loc tp = 
+    match tp with 
+      | <:ctyp< { $list:l$ } >> -> iter_over_fields_ _loc l 
+      | _ -> raise BadType
+
+  let type_of_the_structure _loc tp = 
+    match tp with 
+      | Ast.TyDcl (loc, id, e, ty, []) -> Ast.TyDcl (loc, id ^ "_structure", e, iter_over_fields _loc ty, []) 
+      | _ -> raise BadType
 
 EXTEND Gram
     str_item: 
@@ -27,7 +50,9 @@ EXTEND Gram
 	 
 	 <:str_item<
 	     (* add a _id field *) 
-              type $tds$;   >>
+      type $tds$;  
+      type $type_of_the_structure _loc tds$; 
+	>>
       ]	  
     ];
   END

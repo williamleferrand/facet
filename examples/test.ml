f9f52691dcccbad619987c52e9336a6b4f9b66b5
wleferrand <william@corefarm.com>
2011-03-30 19:30:24 -0700

    progressing ..

--- examples/test.ml
+++ examples/test.ml
@@ -1,23 +1,41 @@
 (* Just a test *)
 
 (* Best practise: write the first accessed labels first *)
-
+ 
+module Lifesaver = 
+  struct 
+    type 'a t = unit 
+  end
 
 module type ELT = 
   sig
     type t
-    type t_structure 
+    type structure__t__ 
+
+    val lifesaver : t Lifesaver.t 
+    val insert__t__ : t Lifesaver.t -> structure__t__ -> t -> unit  
   end
 
-module E =
-  struct 
-    type t = 
-	{
-	  
-	  compensation: bool; 
 
-	  keywords : string list ; 
-	  	  
-	} with facet
-	    
-  end
+type t = 
+    {
+      
+      compensation : bool ; 
+      keywords: string list ;
+      
+    } with facet
+	
+let lifesaver = ()
+  
+  (*
+let s= create__t__ ()
+  
+let e = { compensation = true; 
+	  keywords = [ "ocaml"; "cool" ] } ;; 
+
+insert__t__ () s e ;;
+
+create_test ~test:"coucou" () 
+  *)
+
+let c :int = search__t__ 
--- lib/syntax/pa_facet.ml
+++ lib/syntax/pa_facet.ml
@@ -23,7 +23,50 @@ struct
   (* Misc helpers ********************************************************************)
 
   exception BadType
+
+  (* Create the search function ****************************************************)
+  (* ($lid:label$ = `All) *)
+    (*
+  let rec build_pattern _loc id ty = 
+    match ty with 
+      | <:ctyp< $lid:label$ : $lid:_$ >> -> [ <:patt< ? ($lid:label$ = `All) >> ] 
+      | <:ctyp< $lid:label$ : $lid:_$; $ty$ >> -> [ <:patt< $build_pattern _loc id ty$ >> ] 
+    *)										
+
+    let rec create_search_by_field _loc id ty = 
+    match ty with 
+      | <:ctyp< $lid:label$ : $_$ >> -> <:expr< fun [ ? $lid:label$ -> () ] >>
+      | <:ctyp< $lid:label$ : $_$ ; $ty$ >> -> <:expr< fun [ ? $lid:label$ -> $create_search_by_field _loc id ty$ ] >>
+	
+	
+    let create_search _loc id ty = 
+      <:str_item< value $lid:"search__"^id^"__"$ l =
+      $create_search_by_field _loc id ty$ 
+      >>
+
+  (* Create the insert function ****************************************************)
     
+  let rec create_insert_by_field _loc id ty = 
+    match ty with 
+      | <:ctyp< $lid:label$ : bool >> | <:ctyp< $lid:label$ : string >> -> 
+	<:expr< fun e h -> Hashtbl.add h e.$lid:label$ e >>
+      | <:ctyp< $lid:label$ : list string >> -> 
+	<:expr< fun e h -> List.iter (fun s -> Hashtbl.add h s e) e.$lid:label$ >>
+      |  <:ctyp< $lid:label$ : bool ; $ty$ >> -> 
+      <:expr< 
+	fun e h -> 
+	  let h = 
+	    try 
+	      Hashtbl.find h e.$lid:label$ 
+	    with [ Not_found -> let nt = Hashtbl.create 0 in do { Hashtbl.add h e.$lid:label$ nt; nt } ] in  
+	  $create_insert_by_field _loc id ty$ e h >>
+
+  let create_insert _loc id ty =
+    	<:str_item< value $lid:("insert__"^id^"__")$ l h e = 
+           $create_insert_by_field _loc id ty$ e h
+	>>
+
+	    
   (* Create the type that handles the weak pointers **********************************)
 
   let rec iter_over_fields_ _loc tp = 
@@ -31,15 +74,18 @@ struct
       | <:ctyp< $lid:_$ : string >> | <:ctyp< $lid:_$ : list string >> -> <:ctyp< Hashtbl.t string (Weak.t t) >>  
       | <:ctyp< $lid:_$ : bool >> -> <:ctyp< Hashtbl.t bool (Weak.t t) >>  
       | <:ctyp< $lid:_$ : bool; $tp$ >> -> <:ctyp< Hashtbl.t bool ($iter_over_fields_ _loc tp$) >>  
+       | _ -> raise BadType
 
   let iter_over_fields _loc tp = 
     match tp with 
-      | <:ctyp< { $list:l$ } >> -> iter_over_fields_ _loc l 
+      | <:ctyp< { $list:l$ } >> -> l, iter_over_fields_ _loc l 
       | _ -> raise BadType
 
   let type_of_the_structure _loc tp = 
     match tp with 
-      | Ast.TyDcl (loc, id, e, ty, []) -> Ast.TyDcl (loc, id ^ "_structure", e, iter_over_fields _loc ty, []) 
+      | Ast.TyDcl (loc, id, e, ty, []) -> 
+	let fields, ty = iter_over_fields _loc ty in
+	id, fields, Ast.TyDcl (loc, "structure__"^id^"__", e, ty, []) 
       | _ -> raise BadType
 
 EXTEND Gram
@@ -47,11 +93,18 @@ EXTEND Gram
     [
       [ 
 	 "type"; tds = type_declaration; "with" ; "facet" ->
-	 
+	 let id, fields, structure_type = type_of_the_structure _loc tds in
 	 <:str_item<
-	     (* add a _id field *) 
+	   
       type $tds$;  
-      type $type_of_the_structure _loc tds$; 
+      (* Create the t_structure type *)
+      type $structure_type$; 
+      (* Builds the create function *)
+      value $lid:"create__" ^ id ^"__"$ () = Hashtbl.create 0 ;
+      (* Builds the insert function *) 
+      $create_insert _loc id fields$ ; 
+      (* Builds the search function *)
+      $create_search _loc id fields$ ; 
 	>>
       ]	  
     ];
